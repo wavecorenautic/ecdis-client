febddf876d4564f7af8643c98c52971388183251
Markus Barenhoff <mbarenh@alios.org>
2015-01-22 07:29:24 +0100

    implemented SwitchClock

--- src/Wavecore/ECDIS/Types.hs
+++ src/Wavecore/ECDIS/Types.hs
@@ -23,38 +23,62 @@ data Clock =
         , _bClockTZ :: Behavior TimeZone
         }
 
-clock :: Behavior UTCTime -> Behavior TimeZone -> Clock
+clock :: Behavior UTCTime -> Behavior TimeZone -> Reactive Clock
 clock bUTCTime bLocalTimeZone =
   let bUTCLocal = fmap (utcToLocalTime utc) bUTCTime
       bTZ = bLocalTimeZone 
       bLT = utcToLocalTime <$> bTZ <*> bUTCTime
-  in Clock {
-      _bClockUTCTime = fmap localTimeOfDay bUTCLocal,
-      _bClockUTCDate = fmap localDay bUTCLocal,
-      _bClockLocalTime = fmap localTimeOfDay bLT,
-      _bClockLocalDate = fmap localDay bUTCLocal,
+  in do
+    _but <- mkOnChange $ fmap localTimeOfDay bUTCLocal
+    _bud <- mkOnChange $ fmap localDay bUTCLocal
+    _blt <- mkOnChange $ fmap localTimeOfDay bLT
+    _bld <- mkOnChange $ fmap localDay bLT
+    return $ Clock {
+      _bClockUTCTime = _but,
+      _bClockUTCDate = _bud,
+      _bClockLocalTime = _blt,
+      _bClockLocalDate = _bld,
       _bClockTZ = bTZ
     }
 
+  
 data SwitchClock =
-  SwitchClock { _bSwitchClockTime :: Behavior TimeOfDay
-              , _bSwitchClockDate :: Behavior Day
-              , _bSwitchClockTZ   :: Behavior TimeZone
+  SwitchClock { _bSwitchClockTime  :: Behavior TimeOfDay
+              , _bSwitchClockDate  :: Behavior Day
+              , _bSwitchClockTZ    :: Behavior TimeZone
+              , _bSwitchClockIsUTC :: Behavior Bool
               }
 
 switchClock :: Behavior UTCTime -> Behavior TimeZone -> Event () ->
                Reactive SwitchClock
-switchClock bT bTZ e =
-  let cl = clock bT bTZ
-  in do
-    bIsUTC <- collectE (\_ s -> (not s, not s)) True e
-    let onUTC :: Behavior a -> Behavior a -> Reactive (Behavior (Behavior a))
-        onUTC a b = hold a $ fmap (\isUTC -> if isUTC then a else b) bIsUTC
-    bDisplayTime <- onUTC (_bClockUTCTime cl) (_bClockLocalTime cl) >>= switch
-    bDisplayDate <- onUTC (_bClockUTCDate cl) (_bClockLocalDate cl) >>= switch
-    bDisplayTZ <- onUTC (pure utc) (_bClockTZ cl) >>= switch
-    return $ SwitchClock {
-      _bSwitchClockTime = bDisplayTime,
-      _bSwitchClockDate = bDisplayDate,
-      _bSwitchClockTZ = bDisplayTZ
-      }
+switchClock bT bTZ e = do
+  cl <- clock bT bTZ
+  bIsUTC <- collectE (\_ s -> (not s, not s)) True e
+  let onUTC :: Behavior a -> Behavior a -> Reactive (Behavior (Behavior a))
+      onUTC a b = hold a $ fmap (\isUTC -> if isUTC then a else b) bIsUTC
+  bDisplayTime <- onUTC (_bClockUTCTime cl) (_bClockLocalTime cl)
+                  >>= switch >>= mkOnChange
+  bDisplayDate <- onUTC (_bClockUTCDate cl) (_bClockLocalDate cl)
+                  >>= switch >>= mkOnChange
+  bDisplayTZ <- onUTC (pure utc) (_bClockTZ cl)
+                >>= switch >>= mkOnChange                                    
+  return $ SwitchClock {
+    _bSwitchClockTime = bDisplayTime,
+    _bSwitchClockDate = bDisplayDate,
+    _bSwitchClockTZ = bDisplayTZ,
+    _bSwitchClockIsUTC = fmap ((==) utc) bDisplayTZ
+    }
+
+mkOnChange :: Eq a => Behavior a -> Reactive (Behavior a)
+mkOnChange b = do
+  b0 <- sample b
+  hold b0 $ snapshot (\b1 _ -> b1) (onChange b) b
+  
+  
+onChange :: Eq a => Behavior a -> Event a
+onChange b =
+  filterJust $
+  snapshot (\old new -> if (old == new)
+                        then Nothing
+                        else Just new) (updates b) b
+                                                            
